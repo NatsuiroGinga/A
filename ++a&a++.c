402d9b701aee19ad488d66b15ace33c9167de1bb
NatsuiroGinga <63488856+NatsuiroGinga@users.noreply.github.com>
2021-02-17 16:20:25 +0800

    Add files via upload

---
+++ ++a&a++.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int i,x;
+	
+	i=1;
+	x=1;
+	x=i++;
+	printf("%d",x);
+	
+	printf("%d",i);
+	
+	printf("++a=%d",++a);
+	
+	printf("a=%d",a);
+	return 0;
+}
---
+++ rte.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+void swap(int a, int b);
+int main()
+{
+    int a = 5;
+    int b = 6;
+    swap(a,b);
+    printf("%d-%d\n", a, b);
+    return 0;
+}
+void swap(int a, int b)
+{
+    int t = a;
+    a = b;
+    b = t;
+}
