01bcc9d99a33a23418ab64f42ff55ad67678e130
NatsuiroGinga <63488856+NatsuiroGinga@users.noreply.github.com>
2021-02-26 23:30:53 +0800

    Update and rename 奇偶个数.c to 多.c 多.c

--- 多.c 多.c
+++ 多.c 多.c
@@ -5,13 +5,9 @@ int main()
 	int count1=0;
 	int count2=0;
 	
-	
-	
     do{
-    	
     	scanf("%d",&number);
-    	if( number>0 && number<100000){
-		
+    	if( number>0 && number<100000){	
     	u=number%2;
     	if(u==0){
     		count1++;
@@ -20,7 +16,6 @@ int main()
 		}
 		}
 	}while(number!=-1 );
-    	
 	
 	printf("%d %d",count2,count1);
 	return 0;
